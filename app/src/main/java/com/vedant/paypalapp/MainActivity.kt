283e77865dea1c8b0a33cfc5526516d3310d8a72
Vedant Tiwari <tiwarivedant.1690@gmail.com>
2022-01-13 23:43:34 +0530

    used volley library

--- app/src/main/java/com/vedant/paypalapp/MainActivity.kt
+++ app/src/main/java/com/vedant/paypalapp/MainActivity.kt
@@ -5,28 +5,35 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private var sharedPrefs: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnadd.setOnClickListener {
-            sharedPrefs = getSharedPreferences("addData", Context.MODE_PRIVATE)
-            val myEditor = sharedPrefs?.edit()
-            myEditor?.putString("product_name", namepf.text.toString())
-            myEditor?.apply()
-            Toast.makeText(this@MainActivity,"product saved",Toast.LENGTH_SHORT).show()
+        button.setOnClickListener {
+            val serverURL = "http://192.168.24.112/PhpProject1/text_file.php"
+            val requestQ: RequestQueue = Volley.newRequestQueue(this@MainActivity)
+            val stringRequest = StringRequest(Request.Method.GET, serverURL,
+                { response ->
+                    textView.text = response
 
+                }, { error ->
+                    textView.text = error.message
+                })
+
+            requestQ.add(stringRequest)
         }
 
-        button2.setOnClickListener {
-            getProject.text = sharedPrefs?.getString("product_name","nothing")
-        }
 
     }
+
+
 }
